0d824116318fb16164507934a0253bbac1039a16
최효주 <siisi1324@naver.com>
2025-01-26 21:06:25 +0900

    second commit

--- practice/0124/ws_5_5.py
+++ practice/0124/ws_5_5.py
@@ -1,13 +1,16 @@
 # 아래 함수를 수정하시오.
-def even_elements():
+def even_elements(my_list):
     new_arr=[]
     for i in my_list:
-        if i%2!=0:
-            new_arr.extend(i)
+        if i%2==0:
+            new_arr.extend([i])
         else:
-            my_list.pop()
+            my_list.pop(my_list.index(i))
+    return new_arr
 
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 result = even_elements(my_list)
 print(result)
+
+
