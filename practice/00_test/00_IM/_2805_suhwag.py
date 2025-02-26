9e984f7c359ca84b795794e941ddb9576f7baaf3
최효주 <siisi1324@naver.com>
2025-02-27 00:31:52 +0900

    good

--- practice/00_test/00_IM/_2805_suhwag.py
+++ practice/00_test/00_IM/_2805_suhwag.py
@@ -1,14 +1,9 @@
 # 홀수형 N*N의 농장이 있는데, 딱 맞는 마름모의 점수의 합을 구하라.
 
 def solve(arr, n):
-    cnt = 0
-    r, c = 0, n//2
+    num_sum = 0
+    for i in range(n//2-1):
 
-    while c < n:
-        cnt += arr[r][c]
-        r += 1
-        c -= 1
-        arr[r][c:c+2]
 
 
 
@@ -19,3 +14,27 @@ for tc in range(1, T+1):
     arr = [list(map(int, input().split())) for _ in range(N)]
     result = solve(arr, N)
     print(f'#{tc} {result}')
+
+
+    # 농작물 수확하기
+
+T = int(input())  # test case
+
+for tc in range(1, T + 1):  # test case 만큼 반복
+
+    N = int(input())  # 농장의 크기를 받음
+    n = N // 2 + 1  # 농장 중간의 행 번호를 설정.
+    harvest = 0  # 총 수확량을 설정
+    mdl_sum = 0  # 중간 행 수확량을 설정
+
+    farm = [list(map(int, input())) for _ in range(N)]  # 전체 농장을 받음
+
+    for i in range(n):  # 농장 중간 바로 위 행까지 실행
+        for j in range(n - i - 1, n + i):  # 중간 열에서 좌우로 1칸씩 늘려가며 수확
+            harvest += farm[i][j]  # 수확해서 총 수확량에 저장
+            harvest += farm[N - i - 1][j]  # 밑에서부터 올라오면서도 진행
+
+    for k in range(N):  # 중간 행 수확
+        harvest -= farm[n - 1][k]  # 중간 행을 두 번 수확했으므로 겹치는 양을 빼줌.
+
+    print(f'#{tc} {harvest}')
